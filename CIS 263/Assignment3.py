458114a63f17f76ef1f9469c63587331829e8d73
Joshua Newberry <joshuamnewberry@gmail.com>
2025-02-08 20:53:31 -0500

    first version of assignment - halfway done

---
+++ CIS 263/Assignment3.py
@@ -0,0 +1,99 @@
+from typing import Any
+
+class Queue:
+    def __init__(self, lst:list = None) -> None:
+        if lst == None:
+            lst = []
+        if type(lst) != list:
+            return TypeError
+        self.__lst = lst
+    
+    @property
+    def queue(self) -> list:
+        return self.__lst
+    @queue.setter
+    def queue(self, lst:list) -> None:
+        if type(lst) != list:
+            return TypeError
+        self.__lst = lst
+    
+    @property
+    def size(self) -> int:
+        return len(self.__lst)
+    
+    @property
+    def front(self) -> Any:
+        if self.size == 0:
+            raise IndexError
+        return self.__lst[0]
+    
+    @property
+    def rear(self) -> Any:
+        if self.size == 0:
+            raise IndexError
+        return self.__lst[self.size-1]
+    
+    def add(self, element:Any) -> None:
+        self.__lst.append(element)
+    
+    def remove(self, num:int = 1) -> Any|list:
+        try:
+            if num > self.size:
+                raise ValueError
+        except ValueError:
+            print("Not enough values in the queue")
+        lst = []
+        for _ in range (0, num):
+            lst.append(self.__lst.pop(0))
+        if len(lst) == 1:
+            return lst[0]
+        return lst
+
+class treeNode:
+    def __init__(self, ele, left = None, right = None) -> None:
+        self.value = ele
+        self.left = left
+        self.right = right
+
+class BinarySearchTree:
+    def __init__(self, head = None, length = 0) -> None:
+        self.head = head
+        self.length = length
+    
+    def __str__(self) -> str:
+        return self.level_order
+
+    def add_node(self, new_node:treeNode|int|float) -> None:
+        if type(new_node) != treeNode:
+            if type(new_node) != int and type(new_node) != float:
+                raise TypeError("Must be type treeNode, int, or float")
+            new_node = treeNode(new_node)
+        if self.head is None:
+            self.head = new_node
+            self.length = 1
+        else:
+            curr = self.head
+            while True:
+                if curr.value == new_node.value:
+                    raise ValueError("Must be a unique number")
+                if curr.value > new_node.value:
+                    if curr.left is not None:
+                        curr = curr.left
+                    else:
+                        curr.left = new_node
+                        self.length += 1
+                        return
+                else:
+                    if curr.right is not None:
+                        curr = curr.right
+                    else:
+                        curr.right = new_node
+                        self.length += 1
+                        return
+    
+    def level_order(self) -> str:
+        curr = self.head
+        nodes_traversed = 0
+        queue = Queue()
+        while nodes_traversed < self.length and queue.size > 0:
+            curr
