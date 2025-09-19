b9f12a86ee98dbcc86d5ce48b21bbf0347bc81bd
Joshua Newberry <joshuamnewberry@gmail.com>
2025-09-18 23:07:55 -0400

    Potentially working - starting testing

--- CIS 343/Lab2_ASTprinter/tool/generate_ast.py
+++ CIS 343/Lab2_ASTprinter/tool/generate_ast.py
@@ -5,24 +5,29 @@ import os
 def define_ast(output_dir, base_name, types):
     # Generate the base AST class
     with open(os.path.join(output_dir, f"{base_name.lower()}.py"), "w") as f:
-        f.write(f"class {base_name}:\n")
-        
-
+        f.write(f"class {base_name}:")
+        f.write(f"\n    pass")
 
         for type in types:
-            pass
-        
-
-
-        f.write(define_type(base_name.strip(), class_name.strip(),\
-        [field.strip() for field in fields]))
-        f.write("\n")
+            class_name, fields = type.split(":")
+            class_name = class_name.strip()
+            fields = [field.strip() for field in fields.split(",")]
+            
+            class_definition = define_type(base_name, class_name, fields)
+            f.write(class_definition)
 
 def define_type(base_name, class_name, fields):
-    ret = None
-    for field in fields:
-        pass
-    return ret
+    class_line = f"\n\nclass {class_name}({base_name}):"
+    if fields:
+        args = ", ".join(fields)
+        init_line = "\n    def __init__(self, " + args + "):"
+        body = ""
+        for field in fields:
+            body += "\n        self." + field + " = " + field
+    else:
+        init_line = "\n    def __init__(self):"
+        body = "\n        pass"
+    return class_line + init_line + body
 
 if __name__ == "__main__":
     if (len(sys.argv) != 2):
@@ -35,3 +40,4 @@ if __name__ == "__main__":
         "Literal : value",
         "Unary : operator, right"
     ])
+    print("Generated expr.py")
